c00049b966707f0ea7db41f0057e5d31042c9b9f
Catalin Criveteanu <catalin@equilobe.com>
2014-11-11 14:52:56 +0200

    ...

--- E2E Testing Demo/Test/E2E/localhost.js
+++ E2E Testing Demo/Test/E2E/localhost.js
@@ -1,15 +1,34 @@
 ﻿
-casper.test.begin('Go To localhost without Wait', 2, function suite(test) {
-    casper.start("https://www.boardprospects.com/login", function () {
-        casper.waitForSelector("div[class=login-form-main]", function () {
-            test.assertTitle("Login | BoardProspects", "Login Title is OK!");
-            test.assertExists('div[class=login-form-main]', "We have the login form");
+casper.test.begin('Go To localhost without Wait', function suite(test) {
+    casper.start("http://localhost/e2edemo", function () {
+        casper.waitForSelector("div[class=jumbotron]", function () {
+            test.assertTitle("Home Page - My ASP.NET Application", "Login Title is OK!");
+            test.assertExists('a[id=loginLink]', "We have the login link");
 
-            this.fillSelectors('form.banner-login-panel-main', {
-                'input[name="email"]': 'catalin.criveteanu@equilobe.com',
-                'input[name="password"]': '!QAZ2wsx'
-            }, true);
+            this.click('a[id=loginLink]');
+
+
         });
+    });
+
+    casper.then(function login() {
+
+        casper.waitForSelector("section[id=loginForm]", function () {
+            casper.waitForSelector("form.js-loginForm", function () {
+                this.fillSelectors('form.js-loginForm', {
+                    'input[name=Email]': 'admin@admin.com',
+                    'input[name=Password]': '!QAZ2wsx'
+                }, true);
+            })
+        });
+
+    });
+
+    casper.then(function checkLogin() {
+
+        casper.waitForSelector("a[id=logoutLink]", function () {
+            test.assertExists('a[id=logoutLink]', "We have loggedin!");
+        })
     });
 
     casper.run(function () {
